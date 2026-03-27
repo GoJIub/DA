79047718cf1ca6907c96996c5c395b3babae6641
Zhenia <zhenia.saharow@yandex.ru>
2026-03-28 00:36:21 +0300

    Correct search & add insert

--- 2/main.cpp
+++ 2/main.cpp
@@ -20,6 +20,32 @@ public:
         head->left = head;
     }
 
+    bool insert(std::string key, uint64_t value) {
+        Node* f = search(key);
+        if (key == f->key) return false;
+        int newBit = 0;
+        while (getBit(key, newBit) == getBit(f->key, newBit)) ++newBit;
+        Node* prev = head;
+        Node* cur = head->left;
+        while (cur->bit < newBit) {
+            if (cur->bit <= prev->bit) break;
+            prev = cur;
+            if (getBit(key, cur->bit) == 1) cur = cur->right;
+            else cur = cur->left;
+        }
+        Node* newNode = new Node(key, value, newBit);
+        if (prev->right == cur) prev->right = newNode;
+        else prev->left = newNode;
+        if (getBit(key, newBit) == 1) {
+            newNode->right = newNode;
+            newNode->left = cur;
+        } else {
+            newNode->left = newNode;
+            newNode->right = cur;
+        }
+        return true;
+    }
+
 private:
     Node* head;
     
@@ -31,12 +57,12 @@ private:
     }
 
     Node* search(std::string key) {
-        return search(head->left, key, head);
+        return search(head->left, key, -1);
     }
 
-    Node* search(Node* cur, std::string key, Node* prev) {
-        if (cur->bit <= prev->bit) return cur;
-        if (getBit(key, cur->bit) == 1) return search(cur->right, key, cur);
-        else return search(cur->left, key, cur);
+    Node* search(Node* cur, std::string key, int bit) {
+        if (cur->bit <= bit) return cur;
+        if (getBit(key, cur->bit) == 1) return search(cur->right, key, cur->bit);
+        else return search(cur->left, key, cur->bit);
     }
 };
